2f9f71495c2cd5ea0e127702ea8117095d512f5e
Derek <derek@dinsmore-design.com>
2016-10-25 16:08:47 -0700

    Pulling in posts from collections
    * Adding images to user collections
    * Adding dynamic data to posts
    
    KNOWN BUGS:
    
    * User images aren’t pulling in correctly - package is deprecated. Code
    has been commented out until fix is made - default image is shown on
    front-end.

--- client/common.js
+++ client/common.js
@@ -1,6 +1,20 @@
-Meteor.startup(function(){
-	AccountsEntry.config({
-		homeRoute: '/',
-		dashboardRoute: '/'
-	})
-});
+// Accounts package setups
+Meteor.startup(function () {
+
+    AccountsEntry.config({        
+      homeRoute: '/',            
+      dashboardRoute: '/',
+      passwordSignupFields: 'USERNAME_AND_EMAIL'
+    });
+
+    Accounts.ui.config({
+    	passwordSignupFields: 'USERNAME_AND_EMAIL'
+    });
+ });
+
+
+// Pull user image - BROKEN DUE TO DEPRICATED PLUGIN!
+// Template.registerHelper('getProfileImg', function(userId){
+// 	var imgUrl = UserImages.findOne({userId: userId}).image;
+// 	return imgUrl;
+// });
--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -1,28 +1,28 @@
 Template.profile.events({
 	"submit .edit-profile": function(event) {
-
 		var file = $('#profileImage').get(0).files[0];
 		
-		if (file) {
+		if(file){
 			fsFile = new FS.File(file);
 
 			ProfileImages.insert(fsFile, function(err, result) {
-				if(err) {
+				if(err){
 					throw new Meteor.Error(err);
 				} else {
+
 					var imageLoc = '/cfs/files/ProfileImages/' + result._id;
 
 					UserImages.insert({
-						userId: Meteor.userId,
+						userId: Meteor.userId(),
 						username: Meteor.user().username,
 						image: imageLoc
 					});
+
 					Router.go('/');
 				}
 			});
 		}
 
 		return false;
-
 	}
 });
